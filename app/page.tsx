89bf2cb868812a9e7442825c717e7ef0e33ad189
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-11-05 20:57:52 +0100

    Add theme provider and shadcn components. Dropdown, button and a theme modeToggle. Added new design theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { ModeToggle } from "@/components/modeToggle"
+import { Button } from "@/components/ui/button"
+
 const Home = () => {
   
   return (
-    <div>
-      Home
-    </div>
+    <main className="items-center gap-2 justify-center flex min-h-screen">
+      <ModeToggle />
+      <Button>Click me</Button>
+    </main>
   )
 }
 
